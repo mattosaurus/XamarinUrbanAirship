92c01bb541727263a4e0e047fae2aad9d6fac0f5
Matthew Grant <MatthewG@dda>
2017-05-18 14:59:26 +0100

    Add project files.

---
+++ XamarinUrbanAirship/MyAutopilot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using UrbanAirship;
+using Android.Support.V4.Content;
+
+namespace XamarinUrbanAirship
+{
+    [Register("com.bitscry.xamarinurbanairship.MyAutopilot")]
+    public class MyAutopilot : Autopilot
+    {
+        public override void OnAirshipReady(UAirship airship)
+        {
+            // perform any post takeOff airship customizations
+            airship.PushManager.PushEnabled = true;
+            airship.PushManager.UserNotificationsEnabled = true;
+        }
+
+        public override AirshipConfigOptions CreateAirshipConfigOptions(Context context)
+        {
+            AirshipConfigOptions options = new AirshipConfigOptions.Builder()
+                 .SetInProduction(!BuildConfig.Debug)
+                 .SetDevelopmentAppKey("Your Development App Key")
+                 .SetDevelopmentAppSecret("Your Development App Secret")
+                 .SetProductionAppKey("Your Production App Key")
+                 .SetProductionAppSecret("Your Production App Secret")
+                .Build();
+
+            return options;
+        }
+    }
+}
